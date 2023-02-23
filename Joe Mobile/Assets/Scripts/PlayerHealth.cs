48e8c3a27827bd521cbe98f5d41d44be6359e601
Asuran Delarosa <asu7uu06@gmail.com>
2023-02-23 13:43:15 -0800

    Added new animations and edited level 3

--- Joe Mobile/Assets/Scripts/PlayerHealth.cs
+++ Joe Mobile/Assets/Scripts/PlayerHealth.cs
@@ -13,7 +13,7 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        health = 4 + PlayerPrefs.GetInt("Difficulty");
+        health = 5 + PlayerPrefs.GetInt("Difficulty");
         slider.maxValue = health;
         slider.value = health;
     }
